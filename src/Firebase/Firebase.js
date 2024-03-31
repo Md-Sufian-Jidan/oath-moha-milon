b682ad80d7163e3a2c444b2250c5c32ca76d4193
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-03-31 22:54:35 +0600

    basic project setup complete

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBTD4tHpjkI_ExOTkL8uAbdah1jSpps37g",
+  authDomain: "oath-moha-milon.firebaseapp.com",
+  projectId: "oath-moha-milon",
+  storageBucket: "oath-moha-milon.appspot.com",
+  messagingSenderId: "251911283113",
+  appId: "1:251911283113:web:433a805e13277c968f24b8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
